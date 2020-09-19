87ea9f523fa1fd8bf3cd75f638ac62d554666fae
Larissa129 <70455263+Larissa129@users.noreply.github.com>
2020-09-18 19:49:41 -0600

    Update Dao.js

--- Dao.js
+++ Dao.js
@@ -71,6 +71,114 @@ var fs = require('fs')
 let employees = []
 let managers = []
 let executives = []
+let vendorData = []
+let customerData =[]
+let contractorsData =[]
+
+function vendor(){
+    for (let i=1; i <= 5 ; i++){
+        let id = i
+        let firstName = faker.name.firstName()
+        let middleName = faker.name.firstName()
+        let lastName = faker.name.lastName()
+        let dob = faker.date.between('01-01-1960', '01-01-2005').toLocaleDateString()
+        let phone = faker.phone.phoneNumberFormat()
+        let email = faker.internet.email()
+        let streetAddress = faker.address.streetAddress()
+        let city = faker.address.city()
+        let state = faker.address.state()
+        let zipcode = faker.address.zipCode()
+        let company = faker.company.companyName()
+
+        vendorData.push({
+            "id": id,
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "dob": dob,
+            "phone": phone,
+            "email": email,
+            "street": streetAddress,
+            "city": city,
+            "state":state,
+            "zipcode":zipcode,
+            "company": company
+        });
+        
+
+    }
+
+}
+
+function customer(){
+    for (let i=6; i <= 10 ; i++){
+        let id = i
+        let firstName = faker.name.firstName()
+        let middleName = faker.name.firstName()
+        let lastName = faker.name.lastName()
+        let dob = faker.date.between('01-01-1960', '01-01-2005').toLocaleDateString()
+        let phone = faker.phone.phoneNumberFormat()
+        let email = faker.internet.email()
+        let streetAddress = faker.address.streetAddress()
+        let city = faker.address.city()
+        let state = faker.address.state()
+        let zipcode = faker.address.zipCode()
+        let company = faker.company.companyName()
+
+        customerData.push({
+            "id": id,
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "dob": dob,
+            "phone": phone,
+            "email": email,
+            "street": streetAddress,
+            "city": city,
+            "state":state,
+            "zipcode":zipcode,
+            "company": company
+        });
+        
+
+    }
+
+}
+
+function contractors(){
+    for (let i=11; i <= 15; i++){
+        let id = i
+        let firstName = faker.name.firstName()
+        let middleName = faker.name.firstName()
+        let lastName = faker.name.lastName()
+        let dob = faker.date.between('01-01-1960', '01-01-2005').toLocaleDateString()
+        let phone = faker.phone.phoneNumberFormat()
+        let email = faker.internet.email()
+        let streetAddress = faker.address.streetAddress()
+        let city = faker.address.city()
+        let state = faker.address.state()
+        let zipcode = faker.address.zipCode()
+        let company = faker.company.companyName()
+
+        contractorsData.push({
+            "id": id,
+            "firstName": firstName,
+            "middleName": middleName,
+            "lastName": lastName,
+            "dob": dob,
+            "phone": phone,
+            "email": email,
+            "street": streetAddress,
+            "city": city,
+            "state":state,
+            "zipcode":zipcode,
+            "company": company
+        });
+        
+
+    }
+
+}
 function generateEmployees(){
     for(let id = 16; id <= 35; id++){
         let fname = faker.name.firstName()
